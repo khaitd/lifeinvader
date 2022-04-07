37eedb667e6c2813feecef8472043e810ed7f9bb
Dmitriy <khaitdmitriy@gmail.com>
2022-04-07 18:01:54 -0400

    added post request

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
+
 const Timeline = () => {
+    const [posts, setPosts] = useState([])
+    useEffect(async () => {
+        console.log("rendering timeline")
+        let res = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+        let req = await res.json()
+        setPosts(res)
+   
+    }, [])
+
     return (
         <div> 
             <h2> Timeline </h2>
+            {
+              // posts.map(element)
+            }
         </div>
     )
 }
