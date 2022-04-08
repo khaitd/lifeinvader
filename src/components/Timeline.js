908ae57916e10ba1fcab0a7e66c6e5ca76f26c8a
Dmitriy <khaitdmitriy@gmail.com>
2022-04-08 13:27:08 -0400

    added api call

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,19 +4,39 @@ import { useEffect, useState } from "react";
 const Timeline = () => {
     const [posts, setPosts] = useState([])
     useEffect(async () => {
-        console.log("rendering timeline")
-        let res = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
-        let req = await res.json()
-        setPosts(res)
+       // console.log("rendering timeline")
+        // let res = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+        // let req = await res.json()
+        // setPosts(res)
+        
+        //IIFE
+        (async () => {
+            let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+            let res = await req.json()
+            setPosts(res)
+        })()
+     
    
     }, [])
 
     return (
-        <div> 
+        <div>
+
+
             <h2> Timeline </h2>
             {
-              // posts.map(element)
+                posts.map((element) => {
+                    return (
+                        <div>
+                           <p> Post by {element.username}</p>
+                           <p> {element.content}</p>
+                        </div>
+                    )
+                })
+
+
             }
+           
         </div>
     )
 }
